7f304a15239fc4db17930dac80f4e809bbde726b
Tommy Bi <bisi@thayer.com>
2022-04-11 21:33:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import words from '../data/words';
-import derby from '../data/derby';
-import paw from '../img/paw.png';
+// import derby from '../data/derby';
+// import paw from '../img/paw.png';
 
 (() => {
 
